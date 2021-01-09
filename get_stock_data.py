65f4faf3b21ad84c270696e03855fb3e6cfc0b63
Pim Ziengs <p.ziengs@gmail.com>
2021-01-09 16:45:00 +0100

    Retrieving data and plotting is working correctly now!

--- get_stock_data.py
+++ get_stock_data.py
@@ -1,10 +1,14 @@
 import json
 import requests
 import csv
+from   csv import reader
 import os
+import re
 
 # TODO:
-# Add all data in one big file
+# Add time span in file name
+# Add delta after each iteration, correct for overlap.
+# Append csv file to csv file. 
 
 # Import api_key. (improve)
 with open('./api_key/api_key.json') as json_file:
@@ -21,13 +25,73 @@ slices = [ 'year1month1', 'year1month2', 'year1month3', 'year1month4', 'year1mon
          [ 'year2month4', 'year2month5', 'year2month6', 'year2month7', 'year2month8'] + \
          [ 'year2month9', 'year2month10', 'year2month11', 'year2month12']
 
-for symbol in symbols:
-    for time_period in slices:
-        api_link   = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&' + \
-                     f'symbol={symbol}&interval={interval}&slice={time_period}&apikey={api_key}'
-        data       = requests.get(api_link)
-        filename   = f'./stock_data/{symbol}/{interval}.csv' #-{time_period}
-        # Create directory if it's non-existent yet.
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, 'wb') as f:
-             f.write(data.content)
+
+# TODO:
+# Improve appending to csv file!
+for  symbol in symbols:
+     filename   = f'./stock_data/{symbol}/interval: {interval}.csv' 
+     # Create directory if it's non-existent yet.
+     os.makedirs(os.path.dirname(filename), exist_ok=True) 
+     # All data
+     all_data = []
+     for time_period in slices[0:2]:
+         api_link   = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&' + \
+                      f'symbol={symbol}&interval={interval}&slice={time_period}&apikey={api_key}'
+         orig       = requests.get(api_link).content
+         data       = orig.decode('utf-8').splitlines()
+         header     = data[0]
+         #print(data[0])
+         data       = data[1:]
+         #print(data[0])
+         all_data  += data
+
+     all_data.insert(0, header)
+     print(all_data)
+     with open(filename, "w") as csv_file:
+          # Create the writer object with tab delimiter
+          writer = csv.writer(csv_file, delimiter = '\t')
+          for line in all_data:
+              # Writerow() needs a list of data to be written, so split at all empty spaces in the line 
+              writer.writerow(re.split('\s+',line)) 
+          
+
+
+
+
+
+
+     #    # Split the long string into a list of lines and delete header
+     #    data   = orig.decode('utf-8').splitlines()
+     #    # Header 
+     #    header = data[0]
+     #    # Data without header
+     #    # data   = data.pop(0)
+     #    # Append to all data
+     #    all_data.append(data)
+     # print("data")
+     # print(data)
+     # # 
+     # all_data.insert(0, header)
+
+     # filename   = f'./stock_data/{symbol}/interval: {interval}.csv' 
+     # # Create directory if it's non-existent yet.
+     # os.makedirs(os.path.dirname(filename), exist_ok=True) 
+     # # Open the file for writing
+     # with open(filename, "w") as csv_file:
+     #      # Create the writer object with tab delimiter
+     #      writer = csv.writer(csv_file, delimiter = '\t')
+     #      for line in all_data:
+     #          # Writerow() needs a list of data to be written, so split at all empty spaces in the line 
+     #          writer.writerow(re.split('\s+',line))
+
+     #    #print(data[0])
+     # #    print("Data type")
+     # #    print(data.type())
+     #    #header     = 
+     # #    print("Header")
+     # #    print(header)
+     # #    csv_reader = reader(data)
+     # #    header = next(csv_reader)  
+     #    #print(header)
+     # #    with open(filename, 'ab') as f:
+     # #         f.write(data)
--- plot_data.py
+++ plot_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plot
 
 # TODO:
 # Add results of paper trading algo
@@ -8,11 +10,38 @@ import plotly.express as px
 
 
 
-df = pd.read_csv('stock_data/AAPL/1min.csv')
+df  = pd.read_csv('stock_data/AAPL/interval: 1min.csv')
 
-# Choose columns to plot
-df_select = df[["time", "close"]]
+x = df["time"]
+y = df["close"]
 
-# print(df_select)
-fig = px.line(df_select, x = 'time', y = 'close', title='Apple Share Prices over time (2014)')
+fig = px.line(df, x = "time", y = 'close', title='Apple Share Prices over time (2014)')
 fig.show()
+
+# fig = px.scatter(df, x="time", y="close")
+# # fig.update_xaxes(range=[0, 1500])
+# # fig.update_yaxes(range=[0, 1500])
+
+# df.plot.scatter(x=df["time"], y=df["close"])
+
+
+
+# fig.show()
+
+
+# # 
+
+# fig = go.Figure(go.Scatter(x = df['time'], y = df['close'],
+#                   name='Share Prices (in USD)'))
+
+# fig.update_layout(title='Apple Share Prices over time (2014)',
+#                    plot_bgcolor='rgb(230, 230,230)',
+#                    showlegend=True)
+# fig.show()
+
+# x = df["time"]
+# y = df["close"]
+
+#x=df_select.index df_select.index
+
+
