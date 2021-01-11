b0ddde4bbbfc58d86245297c2f3ec51376786682
Pim Ziengs <p.ziengs@gmail.com>
2021-01-11 15:24:28 +0100

    Busy with creating a workable arima model

--- plot_data.py
+++ plot_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-import matplotlib.pyplot as plot
+
 
 # TODO:
 # Add results of paper trading algo
@@ -17,31 +16,3 @@ y = df["close"]
 
 fig = px.line(df, x = "time", y = 'close', title='Apple Share Prices over time (2014)')
 fig.show()
-
-# fig = px.scatter(df, x="time", y="close")
-# # fig.update_xaxes(range=[0, 1500])
-# # fig.update_yaxes(range=[0, 1500])
-
-# df.plot.scatter(x=df["time"], y=df["close"])
-
-
-
-# fig.show()
-
-
-# # 
-
-# fig = go.Figure(go.Scatter(x = df['time'], y = df['close'],
-#                   name='Share Prices (in USD)'))
-
-# fig.update_layout(title='Apple Share Prices over time (2014)',
-#                    plot_bgcolor='rgb(230, 230,230)',
-#                    showlegend=True)
-# fig.show()
-
-# x = df["time"]
-# y = df["close"]
-
-#x=df_select.index df_select.index
-
-
--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-import json
 
-# Import api_key. (improve)
-with open('./api_key/api_key.json') as json_file:
-     api_key = json.load(json_file)["api_key"]
 
-print(api_key)
+
+dftest = (15.49964652559102, 1.0, 6, 9, {'1%': -4.473135048010974, '5%': -3.28988060356653, '10%': -2.7723823456790124}, -114.9946897099733)
+
+
+results = {"Test statistic": dftest[0], "p-value": dftest[1], "Laggs used": dftest[2], \
+           "Number of observations used": dftest[3], "Critical value 1%": dftest[4]['1%'], \
+           "Critical value 5%": dftest[4]['5%'],"Critical value 10%": dftest[4]['10%'] }
+
+# print(results)
+
+# order = 1
+
+# for order in range(order):
+#     print(order)
+order = 2
+
+d = [1,2,3]
+d = [0]*order + d
+
+print(d)
